640154cbf40c21911676d1ad4fdc8a2fa6e8e678
Rui Campos <mail@ruicampos.org>
2024-01-31 12:57:07 +0000

    Update types.py
    Signed-off-by: Rui Campos <mail@ruicampos.org>

--- core/types.py
+++ core/types.py
@@ -1,6 +1,5 @@
 
 from torch import Tensor
-from typing import Annotated
 from pydantic.functional_validators import AfterValidator
 
 
@@ -12,4 +11,4 @@ def greater_than_zero(value: int) -> int:
     assert value > 0, "Value must be greater than zero"
     return value
 
-PositiveInt = Annotated[int, AfterValidator(greater_than_zero)]
+PositiveInt = int # Annotated[int, AfterValidator(greater_than_zero)]
