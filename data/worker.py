f7e6ab4b2bae190e27b807ba11916de7c675b76c
Rui Campos <mail@ruicampos.org>
2024-02-13 10:44:26 +0000

    remove pickle serialization from celery
    Signed-off-by: Rui Campos <mail@ruicampos.org>

--- data/worker.py
+++ data/worker.py
@@ -6,10 +6,8 @@ celery_app = Celery(
     broker=f"redis://redis:6379/0",
     backend=f"data.backend.CustomBackend://redis:6379/1",
     broker_connection_retry_on_startup=True,
-    result_serializer='pickle',
-
-    # export C_FORCE_ROOT="true", pickle is okay here, closed network 
-    accept_content=['pickle', 'json'],
+    result_serializer='json',
+    accept_content=['json'],
 )
 
 celery_app.conf.worker_max_memory_per_child=12000 # = 12mb, should guarantee that the worker is replaced every time after completion
