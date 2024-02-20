bd7489b63b33178ecfc810423887ffe4419f67fe
Rui Campos <ruifilipedesousacampos@gmail.com>
2024-02-20 16:02:56 +0000

    implement self attention in rust

--- model/__init__.py
+++
@@ -1,7 +0,0 @@
-
-
-from model.__main__ import ModelFactory, gpt2_encoder, SentimentAnalysisModel
-
-__all__ = ["ModelFactory", "gpt2_encoder", "SentimentAnalysisModel"]
-
-
--- src/main.rs
+++ src/main.rs
@@ -1,77 +1,108 @@
-use tch::{nn::{self, Module}, Device};
 
+use tch::nn::{self, Module};
 
+// export LD_LIBRARY_PATH=/workspace/.pyenv_mirror/user/current/lib/python3.12/site-packages/torch/lib
+// export LIBTORCH_USE_PYTORCH=1
 
-fn embedding_table(vs_path: &nn::Path, num_embeddings: i64, embedding_dim: i64) -> impl Module {
-    let ws_init = nn::Init::Randn { mean: 0., stdev: 3. };
+struct ModelParameters {
+    embedding_dimenson: i64,
+    size_of_context_window: i64,
+    size_of_vocabolary: i64,
+    number_of_heads: i64
+}
+
+fn generate_init() -> nn::Init {
+    nn::Init::Randn { mean: 0., stdev: 3. }
+}
+
+fn embedding_table(vs_path: &nn::Path, num_embeddings: i64, embedding_dim: i64) -> impl nn::Module {
     let config = nn::EmbeddingConfig{
         sparse: false,
         scale_grad_by_freq: false,
-        ws_init: ws_init,
-        padding_idx: 1
+        ws_init: generate_init(),
+        padding_idx: 0
     };
     nn::embedding(vs_path, num_embeddings, embedding_dim, config)
 }
 
-fn embedder_module(vs_path: &nn::Path, embedding_dimenson: i64, size_of_context_window: i64) -> impl Module {
+fn embedder_module(vs_path: &nn::Path, hyper_parameters: &ModelParameters) -> impl nn::Module {
 
     let vocabolary = embedding_table(
         vs_path,
-        60_000, 
-        embedding_dimenson
+        hyper_parameters.size_of_vocabolary, 
+        hyper_parameters.embedding_dimenson
     );
 
     let positional_encoding = embedding_table(
         vs_path, 
-        size_of_context_window, 
-        embedding_dimenson
+        hyper_parameters.size_of_context_window, 
+        hyper_parameters.embedding_dimenson
     );
 
-    nn::func(move |xs| vocabolary.forward(xs) + positional_encoding.forward(xs))
+    nn::func(move |x_bc| {
+        vocabolary.forward(&x_bc) + positional_encoding.forward(&x_bc)
+    })
 }
 
 
-fn attention_module(){
+fn self_attention_module(vs_path: &nn::Path, hyper_parameters: &ModelParameters) -> impl nn::Module {
 
+    let n = hyper_parameters.number_of_heads;
+    let c = hyper_parameters.size_of_context_window;
+    let d = hyper_parameters.embedding_dimenson;
+    let q = hyper_parameters.number_of_heads /  hyper_parameters.number_of_heads;
+
+    let projections_1ncq = vs_path.var("projections_1ncq", &[1, n, c, q], generate_init());
+    let metric_tensors_1nqq = vs_path.var("metric_tensors_1nqq", &[1, n, q, q], generate_init());
+    let mixer_1dd = vs_path.var("mixer_1dd", &[1, d, d], generate_init());
+
+    // let sqrt_q: f32 = unsafe { sqrtf32(q) };
+
+    nn::func(move |x_bcd| {
+        // Apply n projections to the input 
+        let x_b1cd = &x_bcd.unsqueeze(1);
+        let x_bncq = &projections_1ncq.matmul(x_b1cd);
+
+        // Use n custom dot products to generate n score tables
+        let x_bnqc = &x_bncq.transpose(-1, -2);
+        let x_bnqq = &x_bncq.matmul(&metric_tensors_1nqq.matmul(x_bnqc));
+    
+        // x_bnqq = &x_bnqq.divide_scalar(sqrt_q);
+
+        let softmaxed_x_bnqq = &x_bnqq.softmax(-1, tch::kind::Kind::Float);
+        let y_bncq = &x_bnqq.matmul(softmaxed_x_bnqq);
+        let y_bcnq = &y_bncq.transpose(1, 2);
+        let y_bcd = &y_bcnq.view([-1, c, d]);
+    
+        y_bcd.matmul(&mixer_1dd)
+    })
 }
 
-fn linear_feedforward() {
-
-}
 
 
-fn transformer_module(){
-
-}
-
-
-struct MetricTensorNetworkParameters {
-    embedding_dimenson: i64,
-    size_of_context_window: i64,
-    size_of_vocabolary: i64
+fn transformer_module(vs_path: &nn::Path, hyper_parameters: &ModelParameters) -> impl nn::Module {
+    nn::seq()
+    .add(self_attention_module(vs_path, hyper_parameters))
 }
 
 
 
-fn metric_tensor_network(vs_path: &nn::Path, hyper_parameters: &MetricTensorNetworkParameters) -> impl Module {
-    nn::seq().add(
-        embedder_module(
-            vs_path,
-            hyper_parameters.embedding_dimenson,
-            hyper_parameters.size_of_context_window,
-        )
-    )
+fn metric_tensor_network(vs_path: &nn::Path, hyper_parameters: &ModelParameters) -> impl nn::Module {
+    let embedding_layer = embedder_module(vs_path, hyper_parameters);
+    nn::seq().add(embedding_layer).add(transformer_module(vs_path, hyper_parameters))
 }
 
 
 fn main() {
-    let vs: nn::VarStore = nn::VarStore::new(Device::Cpu);
+    let vs: nn::VarStore = nn::VarStore::new(tch::Device::Cpu);
     let vs_path = &vs.root();
 
-    let network_config = MetricTensorNetworkParameters{
+    let network_config = ModelParameters {
         embedding_dimenson: 256,
         size_of_context_window: 300,
         size_of_vocabolary: 60_000,
+        number_of_heads: 16,
     };
+
     let _net = metric_tensor_network(vs_path, &network_config);
 }
