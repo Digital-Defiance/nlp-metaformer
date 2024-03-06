729697ef8676ace63ce97a2bd74d68af3fe904ea
Rui Campos <mail@ruicampos.org>
2024-03-06 15:44:43 +0000

    Create capacity.py
    Signed-off-by: Rui Campos <mail@ruicampos.org>

---
+++ pipelines/capacity.py
@@ -0,0 +1,52 @@
+import os
+import asyncio
+import subprocess
+from pathlib import Path
+
+import asyncio
+from contextlib import contextmanager
+from typing import Literal
+from prefect import flow, serve, get_run_logger, task, variables
+from prefect.runner.storage import GitRepository
+import boto3
+
+@task
+def get_client():
+    return boto3.client('autoscaling')
+    
+
+def set_capacity(client, capacity: int) -> None:
+    logger = get_run_logger()
+    response = client.update_auto_scaling_group(
+        AutoScalingGroupName='Infra-ECS-Cluster-spot-gpu-2-d30de970-ECSAutoScalingGroup-Vif1pCjC51Hq',
+        DesiredCapacity = capacity,
+        MaxSize=1,
+        MinSize=0,
+    )
+
+    print(response)
+
+
+@flow
+def main(capacity: int):
+    logger = get_run_logger()
+    client = get_client()
+    set_capacity(client, capacity)
+
+
+if __name__ == "__main__":
+
+    git_repo = GitRepository(
+        url="https://github.com/Digital-Defiance/llm-voice-chat.git",
+        branch = get_active_branch_name(),
+    )
+
+    sentiment_analysis_flow = sentiment_analysis.from_source(
+        entrypoint="pipelines/capacity.py:main",
+        source=git_repo,
+    )
+
+    sentiment_analysis_flow.deploy(
+        name="change-scaling-capacity",
+        work_pool_name = "workpool-prefect",
+    )
