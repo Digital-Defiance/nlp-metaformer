53679c6c18fd1628350c609cf364347ba56ec42e
Rui Campos <mail@ruicampos.org>
2024-03-15 16:53:26 +0000

    wip

--- src/cuda/tensoradd.rs
+++ src/cuda/tensoradd.rs
@@ -1,5 +1,6 @@
 
 
+use libc::c_float;
 use tch::{Device, Kind, TchError, Tensor};
 use torch_sys::C_tensor;
 
@@ -14,11 +15,11 @@ extern "C" {
 }
 
 pub trait AddTensors {
-    fn add_tensors(self, a: &Tensor, b: &Tensor) -> Tensor;
+    fn add_tensors(self, a: &mut Tensor, b: &mut Tensor) -> Tensor;
 }
 
 impl AddTensors for Tensor {
-    fn add_tensors(mut self, a: &Tensor, b: &Tensor) -> Tensor {
+    fn add_tensors(mut self, a: &mut Tensor, b: &mut Tensor) -> Tensor {
         unsafe {
             add_tensors_cuda(
                 self.as_mut_ptr(),
@@ -35,12 +36,13 @@ impl AddTensors for Tensor {
 fn test_add_constant(){
 
     let device = Device::cuda_if_available();
-    let a = Tensor::from_slice(&[5., 25.]).to(device);
-    let b = Tensor::from_slice(&[1., 2.]).to(device);
-    let _c = Tensor::from_slice(&[6., 27.]).to(device);
+
+    let data: &[c_float; 2] = &[5., 25.];
+    let mut a = Tensor::from_slice(data).to(device);
+    let mut b = Tensor::from_slice(data).to(device);
 
     let result = Tensor::zeros(&[1, 2], (Kind::Float, device));
-    let result = result.add_tensors(&a, &b);
+    let result = result.add_tensors(&mut a, &mut b);
 
     
     result.print();
