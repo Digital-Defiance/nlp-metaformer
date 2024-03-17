b5013785fc85da6cd9ddd505cad4d474855626d8
Rui Campos <mail@ruicampos.org>
2024-03-17 10:51:45 +0000

    wip

--- src/cuda/layer_kernel.cu
+++ src/cuda/layer_kernel.cu
@@ -13,16 +13,24 @@ using namespace torch::autograd;
 typedef torch::Tensor *TensorPTR;
 
 template <typename scalar_t> 
-__global__ void metric_attention_forwards_kernel(scalar_t *x_bcd, scalar_t *metric_1nkk) {
-    /// TO DO 
+__global__ void metric_attention_forwards_kernel(
+        scalar_t *x_bcd,
+        scalar_t *metric_1nkk,
+        scalar_t *result_bcd
+) {
+    /// TODO metric_attention_forwards_kernel
     // int i = blockDim.x * blockIdx.x + threadIdx.x;
     // c[i] += a[i] + b[i];
 }
 
 
 template <typename scalar_t> 
-__global__ void metric_attention_backwards_kernel(scalar_t *x_bcd, scalar_t *metric_1nkk) {
-    /// TO DO 
+__global__ void metric_attention_backwards_kernel(
+        scalar_t *x_bcd,
+        scalar_t *metric_1nkk,
+        scalar_t *result_bcd
+) {
+    /// TODO metric_attention_backwards_kernel
     // int i = blockDim.x * blockIdx.x + threadIdx.x;
     // c[i] += a[i] + b[i];
 }
@@ -30,10 +38,12 @@ __global__ void metric_attention_backwards_kernel(scalar_t *x_bcd, scalar_t *met
 
 class MetricTensorAttention : public Function<MetricTensorAttention> {
     public:
-        void forward(
+        static void
+        forward(
             AutogradContext *ctx,
             TensorPTR x_bcd,
             TensorPTR metric_1nkk,
+            TensorPTR result_bcd,
             int b, int c, int d, int n, int k
         ) {
             ctx->save_for_backward({x_bcd, metric_1nkk, result_bcd});
@@ -44,7 +54,7 @@ class MetricTensorAttention : public Function<MetricTensorAttention> {
                     metric_1nkk->data<scalar_t>(),
                     result->data<scalar_t>()
                 );
-            })
+            });
         }
 
         static tensor_list
@@ -53,17 +63,24 @@ class MetricTensorAttention : public Function<MetricTensorAttention> {
             tensor_list grad_outputs
         ) {
             auto saved = ctx->get_saved_variables();
-            auto input = saved[0];
-            auto weight = saved[1];
-            auto bias = saved[2];
+            // x_bcd, metric_1nkk, result_bcd
+            auto x_bcd = saved[0];
+            auto metric_1nkk = saved[1];
+            auto result_bcd = saved[2];
 
-            /// TODO
-        
-            auto grad_output = grad_outputs[0];
-            auto grad_input = grad_output.mm(weight);
-            auto grad_weight = grad_output.t().mm(input);
+            AT_DISPATCH_FLOATING_TYPES(x_bcd->type(), "metric_attention_backwards_kernel", ([&] {
+                metric_attention_backwards_kernel<scalar_t><<<2, 1>>>(
+                    x_bcd->data<scalar_t>(),
+                    metric_1nkk->data<scalar_t>(),
+                    result->data<scalar_t>()
+                );
+            });  
+                  
+            // auto grad_output = grad_outputs[0];
+            // auto grad_input = grad_output.mm(weight);
+            // auto grad_weight = grad_output.t().mm(input);
 
-            return {grad_input, grad_weight};
+            return {}; // {grad_input, grad_weight};
   }
 };
 
