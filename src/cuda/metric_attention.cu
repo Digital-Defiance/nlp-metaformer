b695efa7b61af9b1e51bd948407bea43bb04d563
Rui Campos <mail@ruicampos.org>
2024-04-14 11:45:26 +0000

    wip

--- src/cuda/metric_attention.cu
+++ src/cuda/metric_attention.cu
@@ -16,11 +16,6 @@ using CudaTensorView = torch::PackedTensorAccessor32<scalar_t, D, torch::Restric
 using constants_list = std::vector<at::Tensor>;
 
 
-struct results {
-    int idx;
-    int x;
-};
-
 inline void compute_index(int& idx, int Nx, size_t& x) {
     x = idx % Nx;
     idx = (idx - x) / Nx;
@@ -38,22 +33,19 @@ __global__ void metric_attention_forwards_kernel(
 
     int idx = blockIdx.x * blockDim.x + threadIdx.x; // Global thread index
 
-    if idx > max_global_idx {
-        return 
-    }
+    if (idx > max_global_idx) return;
 
-    
     size_t b;
-    compte_index(idx, q_bnul.size(0), b);
+    compute_index(idx, q_bnul.size(0), b);
 
     size_t n;
-    compte_index(idx, q_bnul.size(1), n);
+    compute_index(idx, q_bnul.size(1), n);
 
     size_t u;
-    compte_index(idx, q_bnul.size(2), u);
+    compute_index(idx, q_bnul.size(2), u);
 
     size_t l;
-    compte_index(idx, q_bnul.size(3), l);
+    compute_index(idx, q_bnul.size(3), l);
 
 
     size_t k = index_table_2l[0][l];
@@ -87,13 +79,36 @@ __global__ void metric_attention_backwards_kernel_p(
     const int max_global_idx
 ) {
 
-    if idx > max_global_idx {
-        return 
-    }
 
-    // TODO: index wizzardy 
-    size_t c_2 = ...;
-    size_t k_2 = ...;
+    int idx = blockIdx.x * blockDim.x + threadIdx.x; // Global thread index
+    if (idx > max_global_idx) return;
+
+
+    size_t b;
+    compute_index(idx, q_bnul.size(0), b);
+
+    size_t n;
+    compute_index(idx, q_bnul.size(1), n);
+
+    size_t u;
+    compute_index(idx, q_bnul.size(2), u);
+
+    size_t l;
+    compute_index(idx, q_bnul.size(3), l);
+
+
+    size_t k = index_table_2l[0][l];
+    size_t k_1 = index_table_2l[1][l];
+
+    size_t c = index_table_2u[0][u];
+    size_t c_1 = index_table_2u[1][u];
+
+ 
+    size_t c_2;
+    compute_index(idx, p_bnck.size(3), c_2);
+
+    size_t k_2;
+    compute_index(idx, p_bnck.size(4), k_2);
 
     if (c_2 == c  && k_2 == k){
         grad_r_bnul__p_bnck[b][n][u][l][c_2][k_2] += M_nl[n][l]*p_bnck[b][n][c_1][k_1];
@@ -135,23 +150,19 @@ class MetricTensorAttention : public Function<MetricTensorAttention> {
         ) {
 
             const auto device = p_bnck.device();
-      
+    
             const auto b = p_bnck.size(0);
             const auto n = p_bnck.size(1);
-            const auto c = p_bnck.size(2);
-            const auto k = p_bnck.size(3);
-
-
             auto index_table_2l = index_tables[0];
             auto index_table_2u = index_tables[1];
-
             const auto l = index_table_2l.size(1);
             const auto u = index_table_2u.size(1);
+            auto r_bnul = torch::zeros((b, n, u, l)).to(device);
 
             const int total_threads = b*n*l*u;
+
             const int number_of_blocks = (total_threads + MAX_THREADS_PER_BLOCK - 1) / MAX_THREADS_PER_BLOCK;
 
-            auto r_bnul = torch::zeros((b, n, u, l)).to(device);
 
             AT_DISPATCH_FLOATING_TYPES(p_bnck.type(), "metric_attention_forwards_kernel", ([&] {
                 metric_attention_forwards_kernel<scalar_t><<<number_of_blocks, MAX_THREADS_PER_BLOCK>>>(
@@ -160,23 +171,25 @@ class MetricTensorAttention : public Function<MetricTensorAttention> {
                     r_bnul.packed_accessor32<scalar_t, 4, torch::RestrictPtrTraits>(),
                     index_table_2l.packed_accessor32<size_t, 2, torch::RestrictPtrTraits>(),
                     index_table_2u.packed_accessor32<size_t, 2, torch::RestrictPtrTraits>(),
-                    
                     total_threads
                 );
             }));
 
             ctx->save_for_backward({  p_bnck, M_nl, index_table_2l, index_table_2u  });
 
+            // TODO: verify this
             auto r_bnu = r_bnul.sum();
-
             return { r_bnu };
         }
 
 
         static tensor_list backward(AutogradContext *ctx, tensor_list grad_outputs) {
 
-            auto grad_network__r_bnul = grad_outputs[0];
-            const auto device = grad_r_bnul.device();
+            auto grad_network__r_bnu = grad_outputs[0];
+
+            const auto device = grad_network__r_bnu.device();
+        
+
             auto saved = ctx->get_saved_variables();
             auto p_bnck = saved[0];
             auto M_nl = saved[1];
